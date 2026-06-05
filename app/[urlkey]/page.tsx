654372dcad5c879bfd269da4a7ef419afdbc87ff
Abhishek Kumar <abhisheksinghsci123@gmail.com>
2026-06-05 08:05:20 +0530

    fixed the cms issue

--- app/[urlkey]/page.tsx
+++ app/[urlkey]/page.tsx
@@ -39,7 +39,7 @@ export async function generateStaticParams() {
     
     return pages
         .map((page: any) => ({
-            urlkey: page.url === "/" ? "" : page?.url ?? "",
+            urlkey:  "privacy-policy" //page.url === "/" ? "" : page?.url ?? "",
         }))
         .filter((item) => item.urlkey);
 
