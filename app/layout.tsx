3a6f8b56b584d73f95e4127ba06c5589f4dddaf4
Abhishek Kumar <abhisheksinghsci123@gmail.com>
2026-06-11 08:38:46 +0530

    fix: fixed the issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
 import { Poppins } from 'next/font/google';
 import ReduxProvider from '@/components/providers/redux-provider'
-// const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] });
+const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] });
 
 
 export const metadata: Metadata = {
@@ -39,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-background">
       <body 
-      // className={`${poppins.className} font-sans antialiased`}
+      className={`${poppins.className} font-sans antialiased`}
       >
         <ReduxProvider>
           <main>
